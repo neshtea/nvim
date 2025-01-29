a2ed27d250b63a4206b2da89374158da3b7f2e2c
Marco Schneider <marco.schneider@active-group.de>
2025-01-29 17:10:18 +0100

    lspconfig: add erlang, hls, nix formatting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,10 +13,23 @@ return {
         })
         lspconfig.lua_ls.setup({})
         lspconfig.hls.setup({})
-        lspconfig.nil_ls.setup({})
+        lspconfig.nil_ls.setup({
+            autostart = true,
+            settings = {
+                ['nil'] = {
+                    formatting = {
+                        command = { "nixfmt" },
+                    },
+                },
+            },
+        })
         lspconfig.ts_ls.setup({})
         lspconfig.metals.setup({})
         lspconfig.rust_analyzer.setup({})
+        lspconfig.erlangls.setup({})
+        lspconfig.hls.setup({
+            filetypes = { 'haskell', 'lhaskell', 'cabal' },
+        })
 
         vim.keymap.set("n", "<leader>l=", vim.lsp.buf.format)
         vim.keymap.set("n", "<leader>laa", vim.lsp.buf.code_action)
